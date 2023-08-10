6ea56637fb36b91fc70bb4bbc77ab97dd7a5b504
Pontus W <131268438+wpontus@users.noreply.github.com>
2023-08-10 14:21:15 +0200

    Add files via upload

---
+++ add-language-pack/Add-Language-pack.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.SYNOPSIS
+  Name: Add-language-pack.ps1
+.DESCRIPTION
+Ändrar start order
+.NOTES
+    Updated: 2017-11-21
+    Release Date: 2017-11-21
+   
+  Author: Pontus Wendt
+#>
+
+#Set Executionpolicy
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
+# Make some Variables
+$ISO = "C:\temp\Add-Language-pack\ISO\sources\install.wim"
+$temp_ISO = "C:\temp\Add-Language-pack\temp_ISO"
+$Language_files = "C:\temp\Add-Language-pack\Language_files\Microsoft-Windows-Client-Language-Pack_x64_sv-se.cab"
+$Scratch = "C:\temp\Add-Language-pack\Scratch"
+
+
+
+
+#What Index Name do you want to modify?
+Dism /Get-ImageInfo /ImageFile:$ISO
+
+$Index_NAme = "Windows 10 Enterprise N"
+
+#Mount your ISO with that specific index and put it in a temporary folder.
+Dism /Mount-Image /ImageFile:$ISO /Name:$Index_NAme /MountDir:$temp_ISO
+
+#Time to add the language pack
+Dism /Image:$temp_ISO /ScratchDir:$Scratch /Add-Package /PackagePath:$Language_files
+
+#Do changes in the mounted Image
+Dism /Commit-Image /MountDir:$temp_ISO
+
+#Unmounting the ISO file and saving it..
+dism /unmount-Wim /MountDir:$temp_ISO /commit
+
+#DONE!
